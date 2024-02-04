f92f924dd9edb1110d444fdcdafe9e3db1521898
Mateus Ito <mateusito10@gmail.com>
2024-02-04 16:22:29 -0300

    chore(api): validate todo creation request body

--- src/infra/http/api/todos/index.post.ts
+++ src/infra/http/api/todos/index.post.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 import { CreateTodoService } from '~/app/services/todo/create-todo';
 import { DrizzleDatabase } from '~/infra/database/drizzle';
 import { type TodoHTTP, TodoViewModel } from '~/infra/http/view-models/todo';
@@ -11,7 +13,14 @@ export default defineEventHandler<Promise<CreateTodoResponse>>(
     const db = DrizzleDatabase.getInstance();
     const createTodoService = new CreateTodoService(db);
 
-    const { title, description } = await readBody(event);
+    const { title, description } = await readValidatedBody(event, (body) => {
+      const schema = z.object({
+        title: z.string().min(1).max(255),
+        description: z.string().max(255).optional(),
+      });
+
+      return schema.parse(body);
+    });
 
     const newTodo = await createTodoService.execute({ title, description });
 
