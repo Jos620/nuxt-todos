f90a12c6f4ee8eccb2df25151d450066700b808b
Mateus Ito <mateusito10@gmail.com>
2024-02-03 21:05:30 -0300

    feat(app): add todo entity

---
+++ src/app/entities/todo.ts
@@ -0,0 +1,45 @@
+import { randomUUID } from 'node:crypto';
+
+interface TodoProps {
+  title: string;
+  description?: string;
+  completedAt?: Date;
+}
+
+export class Todo {
+  public readonly id: string;
+  private props: TodoProps;
+
+  constructor(props: TodoProps, id?: string) {
+    this.props = props;
+    this.id = id || randomUUID();
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  set title(title: TodoProps['title']) {
+    this.props.title = title;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  set description(description: TodoProps['description']) {
+    this.props.description = description;
+  }
+
+  get completedAt() {
+    return this.props.completedAt;
+  }
+
+  get isCompleted() {
+    return !!this.props.completedAt;
+  }
+
+  toggle() {
+    this.props.completedAt = this.props.completedAt ? undefined : new Date();
+  }
+}
