beebadc2efe6ce9021770b7ce22a9b5889f3966b
Mateus Ito <mateusito10@gmail.com>
2024-02-04 16:14:35 -0300

    chore(database): add default todo id generation

--- src/infra/database/drizzle/schema.ts
+++ src/infra/database/drizzle/schema.ts
@@ -1,8 +1,10 @@
+import { randomUUID } from 'node:crypto';
+
 import type { InferSelectModel } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const todosTable = sqliteTable('todos', {
-  id: text('id').unique().primaryKey(),
+  id: text('id').unique().primaryKey().$defaultFn(randomUUID),
   title: text('title').notNull(),
   description: text('description'),
   completedAt: integer('completed_at', { mode: 'timestamp' }),
