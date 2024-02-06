3af2749bfc4c9195edc772d55eae38120f991bc8
Mateus Ito <mateusito10@gmail.com>
2024-02-06 09:46:31 -0300

    test(home): add title and todo table tests

---
+++ tests/e2e/pages/home.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '../.';
+
+test('Home title', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' });
+
+  const heading = page.getByRole('heading', { name: 'Nuxt Todo List' });
+
+  expect(await heading.isVisible()).toBeTruthy();
+});
+
+test('Todo table', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' });
+
+  const todos = page.getByTestId('todo');
+
+  expect(await todos.count()).toBe(3);
+
+  const buyMilkTodo = todos.filter({
+    hasText: 'Buy some milk',
+  });
+
+  expect(await buyMilkTodo.textContent()).toContain(
+    'Remember to check the expiration date',
+  );
+});
