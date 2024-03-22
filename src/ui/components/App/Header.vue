e5cde6dfdf41f7fee3c4c834666c3ef06ccdd791
Mateus Ito <mateusito10@gmail.com>
2024-03-21 23:37:58 -0300

    refactor(layouts): split header into a separate component

---
+++ src/ui/components/App/Header.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const isDark = useDark();
+</script>
+
+<template>
+  <header p-4 border="b-1 muted" bg="white dark:black" sticky top-0 z-1>
+    <div container mx-auto center justify-between>
+      <NuxtLink to="/">
+        <h3>Todo List</h3>
+      </NuxtLink>
+
+      <div center gap-4>
+        <UiButton variant="outline" @click="isDark = !isDark">
+          <div v-if="isDark" i-mdi:white-balance-sunny />
+          <div v-else i-mdi:weather-night />
+        </UiButton>
+
+        <NuxtLink to="/todo/create">
+          <UiButton flex gap-3>
+            New
+            <KBD class="<lg:hidden">/</KBD>
+          </UiButton>
+        </NuxtLink>
+      </div>
+    </div>
+  </header>
+</template>
--- src/ui/layouts/default.vue
+++ src/ui/layouts/default.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { isFocusingInput } from '../lib/window';
 
-const isDark = useDark();
+useDark();
+
 const router = useRouter();
 
 onKeyStroke('/', (e) => {
@@ -15,27 +16,7 @@ onKeyStroke('/', (e) => {
 </script>
 
 <template>
-  <header p-4 border="b-1 muted" bg="white dark:black" sticky top-0 z-1>
-    <div container mx-auto center justify-between>
-      <NuxtLink to="/">
-        <h3>Todo List</h3>
-      </NuxtLink>
-
-      <div center gap-4>
-        <UiButton variant="outline" @click="isDark = !isDark">
-          <div v-if="isDark" i-mdi:white-balance-sunny />
-          <div v-else i-mdi:weather-night />
-        </UiButton>
-
-        <NuxtLink to="/todo/create">
-          <UiButton flex gap-3>
-            New
-            <KBD class="<lg:hidden">/</KBD>
-          </UiButton>
-        </NuxtLink>
-      </div>
-    </div>
-  </header>
+  <AppHeader />
 
   <main p="4 md:y-10 lg:y-20">
     <div container mx-auto>
