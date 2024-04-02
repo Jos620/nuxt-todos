f2cde809db4ab07100995f2b70c187206586e098
Mateus Ito <mateusito10@gmail.com>
2024-04-02 09:33:56 -0300

    test(todos): use get todo service to test complete todo service

--- src/app/services/todo/complete-todo.spec.ts
+++ src/app/services/todo/complete-todo.spec.ts
@@ -4,14 +4,17 @@ import type { TodosRepository } from '@/app/repositories/todo';
 
 import { InMemoryDatabase } from '../../../../tests/database/in-memory-database';
 import { CompleteTodoService } from './complete-todo';
+import { GetTodoService } from './get-todo';
 
 let db: TodosRepository;
 let completeTodoService: CompleteTodoService;
+let getTodoService: GetTodoService;
 
 describe('complete-todo', () => {
   beforeEach(() => {
     db = new InMemoryDatabase().withTodos();
     completeTodoService = new CompleteTodoService(db);
+    getTodoService = new GetTodoService(db);
   });
 
   it('should complete a todo', async () => {
@@ -20,7 +23,7 @@ describe('complete-todo', () => {
 
     await completeTodoService.execute(todo.id);
 
-    const [completedTodo] = await db.getAllTodos();
-    expect(completedTodo.isCompleted).toBe(true);
+    const newTodo = await getTodoService.execute(todo.id);
+    expect(newTodo?.isCompleted).toBe(true);
   });
 });
